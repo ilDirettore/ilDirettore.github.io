5b8c590532fd9d0233fccdafc9ed042b0eed8ef4
Alessandro Benedetti <alessandro@aditsolutions.it>
2016-05-12 07:43:18 +0200

    - controllers

--- js/controllers/note-details.controller.js
+++ js/controllers/note-details.controller.js
@@ -1,3 +1,18 @@
-_app.controller('noteDetailsController', function($scope) {
+_app.controller('noteDetailsController', function($scope,EditingNoteService) {
+
+	$scope.changeEditFlag = function() {
+		$scope.isEditing = ($scope.isEditing==undefined || !$scope.isEditing) // XOR Like
+
+		EditingNoteService.isEditing($scope.isEditing,function(err,result) {
+			if (err)
+				return console.error(err)
+
+			return console.log(result)
+		})
+	}
+
+	$scope.saveNote = function() {
+		EditingNoteService.save();
+	}
 
 })
